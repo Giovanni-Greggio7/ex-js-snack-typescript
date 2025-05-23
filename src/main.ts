766db04e86d83082ed3584a97fdd527d0eb11184
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-05-23 17:47:41 +0200

    Add: bonus snack + snack 2

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,34 @@
-let valore = false 
+let valore: unknown
 
-if(typeof valore === 'string'){
+if (typeof valore === 'string') {
 
-    let valoreMaiscolo = valore.toUpperCase()
-    console.log(valoreMaiscolo)
+  let valoreMaiscolo = valore.toUpperCase()
+  console.log(valoreMaiscolo)
 
-}else if(typeof valore === 'number'){
+} else if (typeof valore === 'number') {
 
-    let perDue = valore * 2
-    console.log(perDue)
+  let perDue = valore * 2
+  console.log(perDue)
 
-}else if(typeof valore === 'boolean'){
+} else if (typeof valore === 'boolean') {
 
-    console.log(valore ? 'Si' : 'No')
+  console.log(valore ? 'Si' : 'No')
+
+} else if (valore === null) {
+
+  console.log('Il dato é vuoto')
+
+} else if (Array.isArray(valore)) {
+
+  console.log(valore.length)
+
+} else if (valore instanceof Promise) {
+
+  valore.then(risultato => console.log(risultato))
+    .catch(error => console.error(error))
+
+} else {
+
+  console.log('Tipo non supportato')
 
 }
