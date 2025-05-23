838afe837b153da6966f83b939e8c848f3a2277d
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-05-23 17:59:29 +0200

    Add: snack 3 + bonus

--- src/type.ts
+++ src/type.ts
@@ -7,3 +7,22 @@ export type Dipendente = {
     readonly emailAziendale: string,
     contratto: 'indeterminato' | 'determinato' | 'freelance'
 }
+
+export type Developer = Dipendente & {
+    livelloEsperienza: 'junior' | 'mid' | 'senior',
+    linguaggi?: string[],
+    certificazioni: string[] 
+}
+
+export type ProjectManager = Dipendente & {
+    teamSize: number | null,
+    budgetGestito?: number,
+    stakeholderPrincipali: string[] 
+}
+
+export type Team = {
+    nome: string,
+    progettoAttuale: string | null,
+    budget: number,
+    membri: [ProjectManager, Developer, ...Developer[]]
+}
